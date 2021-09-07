401b77d18a4e5af1195dd83566d958641e51ff1e
MicheleCortez <michelecortezmaria@gmail.com>
2021-09-07 08:58:45 -0300

    Calculadora em java
    código em java usado em aula para praticar o uso do swing.

---
+++ Calculadora.java
@@ -0,0 +1,30 @@
+package calculadora;
+
+public class Calculadora {
+	
+	//atributos
+	double n1,n2;
+	
+	
+	//metodos
+	double soma(double v1, double v2) {
+	return v1 + v2;	
+	}
+	
+	double sub(double v1, double v2) {
+		return v1 - v2;	
+	}
+	
+	double div(double v1, double v2) {	
+	return v1 / v2;	
+	}
+	
+	double multi(double v1, double v2) {
+	return v1 * v2;	
+	}
+	
+	double resto(double v1, double v2) {
+	return v1 % v2;
+	}
+	
+}
